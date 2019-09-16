0a3b8148793e33b3e600db990b6d831e3b5b562d
Kate Laney <kate.a.laney@gmail.com>
2019-09-16 17:04:34 -0400

    WHY WON'T THE FREAKING AJAX CALL WORK

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,19 +1,40 @@
+$(document).ready(function () {
+
 var topics = ["Fellowship of the Ring", "The Two Towers", "Return of the King", "Miss Peregrine's Home for Peculiar Children", "Black Panther", "Captain Marvel", "Alice in Wonderland"];
 
-$("button").on("click", function () {
+$(document).on("click", ".gif-btn", function() {
+    var searchParam = $(this).attr("data-movie");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchParam + "api_key=4WkQFmyOYQVAFTiG3oCIImwuhOKOHRVa&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        gifDiv.append(p);
+        }
+    });
+  });
 
-    var movie = $(this).attr("data-movie");
+function createButtons() {
+    $("#array-buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+          a.addClass("btn btn-light gif-btn");
+          a.attr("data-name", topics[i]);
+          a.text(topics[i]);
+          $("#array-buttons").append(a);
+        };
+      };
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "4WkQFmyOYQVAFTiG3oCIImwuhOKOHRVa";
+    $("#create-gif").click(function(event) {
+    event.preventDefault();
+        var gif = $("#gif-input-form").val().trim();
+        topics.push(gif);
+        createButtons();
+      });
 
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-        .then(function (response) {
-            console.log(queryURL);
-
-            console.log(response);
-            var results = response.data;
-
-            for (var i = 0; i < results.length; i++) }
+      createButtons();   
+});
